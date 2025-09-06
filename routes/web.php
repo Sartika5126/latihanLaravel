dda89a84b042b6c52551a6158ea68d62379cfb51
unknown <sartika.lp3i.krw24@gmail.com>
2025-09-06 11:03:34 +0700

    latihanLaravel

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return "Hello World dari Laravel!";
+});
+
+Route::get('/nama', function () {
+    return "Hello Guys Nama Saya Sartikaaa!";
+});
