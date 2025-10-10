e5debd95dcbd16da0897179ffa82f6b31d1ba228
Sartika5126 <sartika.lp3i.krw24@gmail.com>
2025-10-10 11:07:59 +0700

    Tambah file register mahasiswa

---
+++ app/Http/Controllers/Auth/StudentRegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class StudentRegisterController extends Controller
+{
+    public function showRegistrationForm()
+    {
+        return view('auth.register-mahasiswa');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|unique:users',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'user', //Calon Mahasiswa
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('ekyc.step1')
+                         ->with('success', 'Registrasi berhasil! Silahkan lengkapi data E-KYC Anda.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
 use App\Http\Controllers\RuanganController;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\Auth\StudentRegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -35,7 +36,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/dosen', [DosenController::class, 'index'])->name('dosen.index');
     Route::post('/dosen', [DosenController::class, 'store'])->name('dosen.store');
     
-    
+    Route::get('/register-mahasiswa', [StudentRegisterController::class, 'showRegistrationForm'])
+    ->name('register.mahasiswa');
+    Route::post('/register-mahasiswa', [StudentRegisterController::class,'register']);
     
 });
 
